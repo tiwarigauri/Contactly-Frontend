df6310f7bfbd47ffdb037c9ac1dbc828280f7d63
tiwarigauri <gauri.uiet@gmail.com>
2024-09-03 01:26:04 +0530

    modified

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -16,12 +16,6 @@ export const Sidebar = () => {
       <div className="sidebar-item">
         <FaCubesStacked className="top-icon" />
       </div>
-      <div className={`sidebar-item ${activeLink === 0 ? " active " : ""}`}
-      onClick={() => setActiveLink(0)}>
-        <Link className="sidebar-link">
-          <FaUser className="icon" /> Profile
-        </Link>
-      </div>
       <div className={`sidebar-item ${activeLink === 1 ? " active " : ""}`}
       onClick={() => setActiveLink(1)}>
         <Link to="/dashboard" className="sidebar-link">
